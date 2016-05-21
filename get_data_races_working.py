d7dbd2de40e2384e4be0dd039e1c29b85fe5ff1e
Gilbert Kipkorir <kipkorirgilbert@gmail.com>
2016-05-21 15:53:10 -0400

    mostly test files

---
+++ get_data_races_working.py
@@ -0,0 +1,57 @@
+import urllib2
+import json 
+
+
+#global variables 
+startYear = 2006
+endYear = 2013
+myData = {}
+
+for year in range(startYear, endYear+1):
+	url = "http://api.census.gov/data/timeseries/healthins/sahie?get=NAME,RACE_DESC,RACECAT,PCTUI_PT&for=state:*&time=" + str(year) 
+	yearData = json.load(urllib2.urlopen(url))
+
+	states = []
+	state_data = {}
+
+	for i in range(1, len(yearData)):
+		# obj_size = len(yearData[i])
+
+		# for j in range(obj_size):
+		# 	category_name = yearData[0][j]
+		# 	category_value = yearData[i][j]
+		# 	state_data[category_name] = category_value
+
+		state_name = yearData[i][0]
+		race_object = {}
+		race_category = yearData[i][2]
+		race_object[race_category] = [yearData[i][3], yearData[i][1]]
+
+		if (state_name in state_data): # state already in dictionary 
+
+			#add new category 
+
+			temp = state_data[state_name]
+			temp[race_category] = [yearData[i][3], yearData[i][1]]
+			state_data[state_name] = temp  
+
+
+		else:
+			state_data[state_name] = race_object
+
+
+	str_year = str(year)
+	myData[str_year] = state_data
+
+	print myData
+
+
+# save the data in a json file 
+
+output_name = "race_Data.json" #"race_data.txt"
+
+with open(output_name, 'w') as outfile:
+    json.dump(myData, outfile)
+
+
+
